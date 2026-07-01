203179c6414e6d4df7577b7860acfe8bff79877f
nswdry <marin.taniguchi25@gmail.com>
2026-07-01 23:24:27 +0900

    [add]Deviseを使ったユーザー認証機能の追加
    deviseの導入、Userモデルを作成し、ストロングパラメータの設定を行いました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :books
+  resources :users, only: [:index, :show]
+  root 'books#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
