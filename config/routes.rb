9b56c11989ee201cb90b6a225f7e7b1b9ce374c9
nswdry <marin.taniguchi25@gmail.com>
2026-08-02 14:39:56 +0900

    [fix]フラッシュメッセージが2回表示されてしまう問題を修正
    - users/indexの重複部分を削除
    - rootをusers#indexに変更することでbooksのフラッシュメッセージの影響を受けないようにしました。

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :books
   resources :users, only: [:index, :show]
-  root 'books#index'
+  root 'users#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
