1bd72bcbc6f4e5c187be51a4d00f77ca634ca16f
nswdry <marin.taniguchi25@gmail.com>
2026-08-11 00:05:33 +0900

    [add]Reportモデルを作成
    - CRUD機能作成
    - 編集・更新・削除の権限設定追加
    - モデルの日本語訳を追加
    - ヘッダーにReport#indexへのリンクを追加

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,6 +2,7 @@
 
 class ReportsController < ApplicationController
   before_action :set_report, only: %i[show edit update destroy]
+  before_action :validate_user, only: %i[edit update destroy]
 
   # GET /reports or /reports.json
   def index
@@ -21,7 +22,7 @@ class ReportsController < ApplicationController
 
   # POST /reports or /reports.json
   def create
-    @report = Report.new(report_params)
+    @report = current_user.reports.build(report_params)
 
     respond_to do |format|
       if @report.save
@@ -67,5 +68,11 @@ class ReportsController < ApplicationController
   # Only allow a list of trusted parameters through.
   def report_params
     params.require(:report).permit(:title, :body)
+  end
+
+  def validate_user
+    return unless @report.user != current_user
+
+    redirect_to reports_path, alert: '自分の投稿ではありません'
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :reports
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
