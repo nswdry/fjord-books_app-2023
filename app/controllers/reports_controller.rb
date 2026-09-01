b89b1257e7bfc58c4de88d8f019398092ba5c56b
nswdry <marin.taniguchi25@gmail.com>
2026-09-01 21:45:08 +0900

    [add]コメント一覧表示時のN+1問題を解消

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -62,7 +62,7 @@ class ReportsController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_report
-    @report = Report.find(params[:id])
+    @report = Report.includes(comments: :user).find(params[:id])
   end
 
   # Only allow a list of trusted parameters through.
