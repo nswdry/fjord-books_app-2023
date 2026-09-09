3e89a1dc5931bf829bccf76354bd0472158b33fc
nswdry <marin.taniguchi25@gmail.com>
2026-09-09 21:46:45 +0900

    [fix]日報・コメント一覧画面の並び順にorderを指定しました。
    - 作成日時順、id順で並び替える

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,7 +4,7 @@ class ReportsController < ApplicationController
   before_action :set_report, only: %i[edit update destroy]
 
   def index
-    @reports = Report.all
+    @reports = Report.order(:created_at, :id)
   end
 
   def show
